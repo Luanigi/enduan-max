6b51f866c8a0db0c84917de0d29fdb73d94f5bab
Luan <89736491+Luanigi@users.noreply.github.com>
2024-05-19 23:31:46 +0200

    test

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configurations
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "enduan-max.firebaseapp.com",
