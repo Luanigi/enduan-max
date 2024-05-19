1ad6fd36a33b2e31618e2e46a1f24abae997bff2
Luan <89736491+Luanigi@users.noreply.github.com>
2024-05-19 22:58:00 +0200

    error fixing

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/Google"
+import GoogleProvider from "next-auth/providers/Google.js"
 
 const handler = NextAuth ({
   // Configure one or more authentication providers
