d9078ee07fa273e8aeb47510e40680f97e0bc958
Luan <89736491+Luanigi@users.noreply.github.com>
2024-05-20 13:14:17 +0200

    google provider path changed

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,10 +1,10 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import google from 'next-auth/providers/google'
 
 const handler = NextAuth ({
   // Configure one or more authentication provider
   providers: [
-    GoogleProvider({
+    google({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
