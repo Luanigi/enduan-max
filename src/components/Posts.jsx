ba55cb9d8c6e5b24dcc2f03621e8b47c6b5ddf44
Luan <89736491+Luanigi@users.noreply.github.com>
2024-05-20 17:52:44 +0200

    this may work

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,21 +1,26 @@
-import {
-    collection,
-    getDocs,
-    getFirestore,
-    orderBy,
-    query,
-  } from 'firebase/firestore';
+import { collection, getFirestore, orderBy, query } from 'firebase/firestore';
   import { app } from '../firebase';
   import Post from './Post';
+  import { useEffect, useState } from 'react';
+
   
-  export default async function Posts() {
-    const db = getFirestore(app);
-    const q = query(collection(db, 'posts'), orderBy('timestamp', 'desc'));
-    const querySnapshot = await getDocs(q);
-    let data = [];
-    querySnapshot.forEach((doc) => {
-      data.push({ id: doc.id, ...doc.data() });
-    });
+  export default function Posts() {
+    const [data, setData] = useState([]);
+  
+    useEffect(() => {
+      const db = getFirestore(app);
+      const q = query(collection(db, 'posts'), orderBy('timestamp', 'desc'));
+      const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        const newData = [];
+        querySnapshot.forEach((doc) => {
+          newData.push({ id: doc.id, ...doc.data() });
+        });
+        setData(newData);
+      });
+  
+      return () => unsubscribe();
+    }, []);
+  
     return (
       <div>
         {data.map((post) => (
