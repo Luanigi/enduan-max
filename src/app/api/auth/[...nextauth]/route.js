0bc083ca5c9c61a5518ec6b9fb5391fc483f03f8
Luan <89736491+Luanigi@users.noreply.github.com>
2024-05-20 13:47:23 +0200

    next_auth secret added

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,14 +2,13 @@ import NextAuth from "next-auth"
 import google from 'next-auth/providers/google'
 
 const handler = NextAuth ({
-  // Configure one or more authentication provider
   providers: [
     google({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
-    // ...add more providers here
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async session({session, token}) {
       session.user.username = session.user.name.split(' ').join('').toLocaleLowerCase();
