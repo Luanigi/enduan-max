0868c6ea6cb0c32b981c8df83c451578053c1b96
Luan <89736491+Luanigi@users.noreply.github.com>
2024-05-20 17:27:33 +0200

    small changes

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,20 +1,26 @@
-import { doc, getDocs, getFirestore, query, collection, orderBy } from 'firebase/firestore'
-import { app } from '../firebase';
-import Post from './Post';
-
-export default async function Posts() {
-  const db = getFirestore(app);
-  const q = query(collection(db, 'posts'), orderBy('timestamp', 'desc'));
-  const querySnapshot = await getDocs(q);
-  let data = [];
-  querySnapshot.forEach((doc) => {
-    data.push({ id: doc.id, ...doc.data() });
-  });
-  return (
-    <div>
-      {data.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
-    </div>
-  )
-}
+import {
+    collection,
+    getDocs,
+    getFirestore,
+    orderBy,
+    query,
+  } from 'firebase/firestore';
+  import { app } from '../firebase';
+  import Post from './Post';
+  
+  export default async function Posts() {
+    const db = getFirestore(app);
+    const q = query(collection(db, 'posts'), orderBy('timestamp', 'desc'));
+    const querySnapshot = await getDocs(q);
+    let data = [];
+    querySnapshot.forEach((doc) => {
+      data.push({ id: doc.id, ...doc.data() });
+    });
+    return (
+      <div>
+        {data.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </div>
+    );
+  }
