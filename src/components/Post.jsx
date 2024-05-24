0682a63a1b5d90b0d05751bbfffa184318af6cfe
Luan <89736491+Luanigi@users.noreply.github.com>
2024-05-24 16:19:50 +0200

    design improvements

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,18 +5,20 @@ import CommentSection from "./CommentSection"
 export default function Post({post}) {
   return (
     <div className="bg-slate-700 my-7 border rounded-md mx-10">
-        <div className="flex items-center p-5 border-bottom border-gray-100">
+        <div className="flex items-center p-5 border-bottom border-gray-100 border-b-2">
             <img src={post.profileImg} alt={post.username} className="h-12 rounded-full object-cover border-2 border-slate-600 p-1 mr-3" />
             <p className="flex-1 font-bold">{post.username}</p>
+            {/* auf click kleine dropdown unten hinzufügen */}
             <HiOutlineDotsVertical className="cursor-pointer h-5"/>
         </div>
 
         <img src={post.image} alt={post.caption} className="object-cover w-full" />
         <LikeSection id = {post.id} />
-        <p className="p-5 truncate">
+        <p className="p-5 border-t-2">
             <span className="font-bold mr-2">{post.username}</span>
-            {post.caption
-        }</p>
+            <br />
+            <p className="">{post.caption}</p>
+        </p>
         <CommentSection id = {post.id} />
     </div>
   )
