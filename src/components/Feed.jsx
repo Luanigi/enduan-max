1228aa3553efa52062b00e98b53f40097388bbce
Luan <89736491+Luanigi@users.noreply.github.com>
2024-05-26 16:40:30 +0200

    made feed section responsive for desktop

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,15 +2,16 @@
 
 import MiniProfile from "./MiniProfile";
 import Posts from "./Posts";
-import { useSession } from 'next-auth/react'
+import { useSession, signIn } from 'next-auth/react'
 
 export default function Feed() {
   const { data: session } = useSession();
   return (
-    <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
+    <>
 
     {session ? (
       <>
+        <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
         <section className="md:col-span-2">
           <Posts />
         </section>
@@ -19,15 +20,44 @@ export default function Feed() {
             <MiniProfile />
           </div>
         </section>
+        </main>
       </>
       ) : (
-        <section className="md:inline-grid md:col-span-1">
-          <div className="fixed w-[380px]">
-            <MiniProfile />
-          </div>
+        <main className="w-2/4 mx-auto">
+        <section className="text-center">
+          <h1 className="text-3xl text-center pt-10">Welcome to Enduan MAX</h1>
+          <p className="w-3/4 text-justify mx-auto p-5">
+            Hey there!   Welcome to Enduan Max, the social 
+            media platform where knowledge meets laughter 
+            and anything goes (almost)! Tired of censored 
+            content and echo chambers? We are too. Here, you 
+            can explore a vast ocean of information, engage 
+            in stimulating discussions, and have a blast 
+            while you're at it.
+          </p>
+
+          <p className="w-3/4 text-justify mx-auto p-5">
+            Enduan Max is built on the foundation of uncensored, 
+            knowledge-based content. Dive deep into diverse topics, 
+            share your unique perspectives, and learn from a vibrant 
+            community. Don't be afraid to ask questions, challenge ideas, 
+            and have a little fun along the way. We encourage healthy 
+            debate, creativity, and a thirst for knowledge. So, buckle up, 
+            unleash your curiosity, and get ready to experience a whole new 
+            way to learn and connect!
+          </p>
+
+          <button
+                onClick={signIn}
+                className="text-blue-500 text-sm flex-1 m-auto font-semibold">
+                    Sign In <span className="text-red-500"> NOW!!</span>
+          </button>
         </section>
+        </main>
       )}
 
-      </main>
+      
+
+      </>
   )
 }
